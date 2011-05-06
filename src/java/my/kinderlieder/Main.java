037c277011c5ea9bb5d6e2e01f940ee6e1004a78
Markus Heberling <markus@tisoft.de>
2011-05-06 19:05:14 +0200

    added list of songs

--- src/java/my/kinderlieder/Main.java
+++ src/java/my/kinderlieder/Main.java
@@ -5,24 +5,57 @@ package my.kinderlieder;
 
 import org.xmlvm.iphone.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main extends UIApplicationDelegate {
 
     @Override
     public void applicationDidFinishLaunching(UIApplication app) {
-        UIWindow window = new UIWindow(UIScreen.mainScreen().getBounds());
+        final UIWindow window = new UIWindow(UIScreen.mainScreen().getBounds());
 
-        UIView mainView = new UIView(window.getFrame());
-        mainView.setBackgroundColor(UIColor.whiteColor);
-        window.addSubview(mainView);
+        final List<SongInfo> songInfos=new ArrayList<SongInfo>();
+        songInfos.add(new SongInfo("Die Gedanken sind frei",NSBundle.mainBundle().pathForResource("die-gedanken-sind-frei-5-strophen-diegedankensindfrei", "pdf")));
+        songInfos.add(new SongInfo("Hänschen klein", NSBundle.mainBundle().pathForResource("haenschen-klein-haenschenkleinc_0", "pdf")));
+        songInfos.add(new SongInfo("Mein Hut der hat drei Ecken", NSBundle.mainBundle().pathForResource("mein-hut-der-hat-drei-ecken-mein-hut-der-hat-drei-ecken-d-dur", "pdf")));
 
-        UIWebView pdfView=new UIWebView(UIScreen.mainScreen().getApplicationFrame());
-        pdfView.setScalesPageToFit(true);
-        String pdfPath = NSBundle.mainBundle().pathForResource("die-gedanken-sind-frei-5-strophen-diegedankensindfrei","pdf");
-        pdfView.loadRequest(NSURLRequest.requestWithURL(NSURL.fileURLWithPath(pdfPath)));
+        UIViewController rootViewController = new UIViewController();
+        final UINavigationController navigationController = new UINavigationController(rootViewController);
+        window.addSubview(navigationController.getView());
 
-        mainView.addSubview(pdfView);
+        UITableView mainView = new UITableView(window.getFrame(), UITableViewStyle.Plain);
+        rootViewController.setView(mainView);
 
-        window.makeKeyAndVisible();
+        mainView.setDataSource(new UITableViewDataSource() {
+            @Override
+            public UITableViewCell cellForRowAtIndexPath(UITableView table, NSIndexPath idx) {
+
+                UITableViewCell cell=new UITableViewCell(UITableViewCellStyle.Default, null);
+                cell.getTextLabel().setText(songInfos.get(idx.getRow()).name);
+                return cell;
+            }
+
+            @Override
+            public int numberOfRowsInSection(UITableView table, int section) {
+                return songInfos.size();
+            }
+        });
+
+        mainView.setDelegate(new UITableViewDelegate() {
+            @Override
+            public void didSelectRowAtIndexPath(UITableView tableview, NSIndexPath indexPath) {
+                UIViewController pdfViewController = new UIViewController();
+                UIWebView pdfView = new UIWebView(window.getFrame());
+                pdfViewController.setView(pdfView);
+                pdfView.setScalesPageToFit(true);
+                pdfView.loadRequest(NSURLRequest.requestWithURL(NSURL.fileURLWithPath(songInfos.get(indexPath.getRow()).pdfPath)));
+
+
+                navigationController.pushViewController(pdfViewController, true);
+            }
+        });
+
+         window.makeKeyAndVisible();
     }
 
     public static void main(String[] args) {
---
+++ src/java/my/kinderlieder/SongInfo.java
@@ -0,0 +1,11 @@
+package my.kinderlieder;
+
+public class SongInfo {
+    public final String name;
+    public final String pdfPath;
+
+    public SongInfo(String name, String pdfPath) {
+        this.name=name;
+        this.pdfPath=pdfPath;
+    }
+}
