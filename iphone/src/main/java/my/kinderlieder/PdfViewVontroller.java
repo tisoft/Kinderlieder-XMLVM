e072d6a9037aa39b874af67620916b670e55ec26
Markus Heberling <markus@tisoft.de>
2011-12-07 18:58:41 +0100

    use UIActionSheet

--- iphone/src/main/java/my/kinderlieder/PdfViewVontroller.java
+++ iphone/src/main/java/my/kinderlieder/PdfViewVontroller.java
@@ -7,129 +7,143 @@ import java.util.Arrays;
 import java.util.List;
 
 class PdfViewVontroller extends RotatingViewController {
-	// these should be local, but can't since we need them inside the delegate
-	private UIBarButtonItem rightBarButtonItem;
-	public UIBarButtonItem repeatButton;
-	private List<UIBarButtonItem> buttonsPlay;
-	private List<UIBarButtonItem> buttonsPauseStop;
-	private List<UIBarButtonItem> buttonsPlayStop;
-	boolean repeat;
+    // these should be local, but can't since we need them inside the delegate
+    private UIBarButtonItem rightBarButtonItem;
+    public UIBarButtonItem repeatButton;
+    private List<UIBarButtonItem> buttonsPlay;
+    private List<UIBarButtonItem> buttonsPauseStop;
+    private List<UIBarButtonItem> buttonsPlayStop;
+    boolean repeat;
+    private SongInfo songInfo;
+    private final UIBarButtonItem playButton;
 
-	public PdfViewVontroller(final SongInfo songInfo, UIWindow window) {
-		setTitle(songInfo.getName());
-		UIWebView pdfView = new UIWebView(window.getFrame());
-		setView(pdfView);
-		pdfView.setScalesPageToFit(true);
-		final NSURL pdfURL = NSURL.fileURLWithPath(songInfo.getPdfPath().getPath());
-		pdfView.loadRequest(NSURLRequest.requestWithURL(pdfURL));
-		if (UIPrintInteractionController.isPrintingAvailable()) {
-			rightBarButtonItem = new UIBarButtonItem(UIBarButtonSystemItem.Action, new UIBarButtonItemDelegate() {
-				public void clicked() {
-					UIPrintInteractionController print = UIPrintInteractionController.sharedPrintController();
-					print.setPrintingItem(pdfURL);
-					print.presentFromBarButtonItem(rightBarButtonItem, true,
-							new UIPrintInteractionController.UIPrintInteractionCompletionHandler() {
+    public PdfViewVontroller(final SongInfo songInfo, UIWindow window) {
+        this.songInfo = songInfo;
+        setTitle(songInfo.getName());
+        UIWebView pdfView = new UIWebView(window.getFrame());
+        setView(pdfView);
+        pdfView.setScalesPageToFit(true);
+        final NSURL pdfURL = NSURL.fileURLWithPath(songInfo.getPdfPath().getPath());
+        pdfView.loadRequest(NSURLRequest.requestWithURL(pdfURL));
+        if (UIPrintInteractionController.isPrintingAvailable()) {
+            rightBarButtonItem = new UIBarButtonItem(UIBarButtonSystemItem.Action, new UIBarButtonItemDelegate() {
+                public void clicked() {
+                    UIPrintInteractionController print = UIPrintInteractionController.sharedPrintController();
+                    print.setPrintingItem(pdfURL);
+                    print.presentFromBarButtonItem(rightBarButtonItem, true,
+                            new UIPrintInteractionController.UIPrintInteractionCompletionHandler() {
 
-								public void completed(UIPrintInteractionController controller, boolean b,
-										NSError nsError) {
-									System.out.println("finished: " + b + ", error: " + nsError);
-								}
-							});
-				}
-			});
-			setHidesBottomBarWhenPushed(false);
-			getNavigationItem().setRightBarButtonItem(rightBarButtonItem);
-		}
+                                public void completed(UIPrintInteractionController controller, boolean b,
+                                                      NSError nsError) {
+                                    System.out.println("finished: " + b + ", error: " + nsError);
+                                }
+                            });
+                }
+            });
+            setHidesBottomBarWhenPushed(false);
+            getNavigationItem().setRightBarButtonItem(rightBarButtonItem);
+        }
 
-		if (Main.getAudioPlayer() != null) {
-			Main.getAudioPlayer().stop();
-		}
-		UIBarButtonItem playButton = new UIBarButtonItem(UIBarButtonSystemItem.Play, new UIBarButtonItemDelegate() {
-			public void clicked() {
-				if ((Main.getAudioPlayer() == null || Main.getAudioPlayer().getCurrentTime() == 0.d)
-						&& Main.library.getMusicInfos(songInfo).size() > 1) {
-					// player is stopped and we have multiple music files
-					UIAlertView selectMusicView = new UIAlertView("Play", "Lied wählen", new UIAlertViewDelegate() {
+        playButton = new UIBarButtonItem(UIBarButtonSystemItem.Play, new UIBarButtonItemDelegate() {
+            public void clicked() {
+                final List<MusicInfo> musicInfos = Main.library.getMusicInfos(songInfo);
+                if (Main.getAudioPlayer() == null && musicInfos.size() > 1) {
+                    String[] titles=new String[musicInfos.size()];
+                    for(int i=0;i<titles.length;i++){
+                        titles[i]=musicInfos.get(i).getName();
+                    }
 
-						@Override
-						public void clickedButtonAtIndex(UIAlertView alertView, int buttonIndex) {
-							Main.setAudioPlayer(AVAudioPlayer.audioPlayerWithContentsOfURL(
-									NSURL.fileURLWithPath(Main.library.getMusicInfos(songInfo).get(buttonIndex).getMusicPath().getPath()), null));
-							Main.getAudioPlayer().prepareToPlay();
-							play();
-						}
-					}, null);
-					for (MusicInfo s : Main.library.getMusicInfos(songInfo)) {
-						selectMusicView.addButtonWithTitle(s.getName());
-					}
+                    // player is stopped and we have multiple music files
+                    UIActionSheet uiActionSheet = UIActionSheet.init("Play", new UIActionSheetDelegate() {
+                        @Override
+                        public void clickedButtonAtIndex(UIActionSheet actionSheet, int buttonIndex) {
+                            Main.setAudioPlayer(AVAudioPlayer.audioPlayerWithContentsOfURL(
+                                    NSURL.fileURLWithPath(musicInfos.get(buttonIndex).getMusicPath().getPath()), null));
+                            Main.getAudioPlayer().prepareToPlay();
+                            play();
 
-					selectMusicView.show();
-				} else {
-					play();
-				}
-			}
+                        }
+                    }, null, null, titles);
+                   uiActionSheet.showFromBarButtonItem(playButton, true);
+                   
+                } else {
+                    play();
+                }
+            }
 
-			private void play() {
-				Main.getAudioPlayer().play();
-				Main.getAudioPlayer().setDelegate(new AVAudioPlayerDelegate() {
-					public void audioPlayerDidFinishPlaying(AVAudioPlayer player, boolean successfully) {
-						player.prepareToPlay();
-						setToolbarItems(new ArrayList<UIBarButtonItem>(buttonsPlay));
-					}
+            private void play() {
+                Main.getAudioPlayer().play();
+                Main.getAudioPlayer().setDelegate(new AVAudioPlayerDelegate() {
+                    public void audioPlayerDidFinishPlaying(AVAudioPlayer player, boolean successfully) {
+                        player.prepareToPlay();
+                        setToolbarItems(new ArrayList<UIBarButtonItem>(buttonsPlay));
+                    }
 
-					public void audioPlayerDecodeErrorDidOccur(AVAudioPlayer player, NSError error) {
-					}
+                    public void audioPlayerDecodeErrorDidOccur(AVAudioPlayer player, NSError error) {
+                    }
 
-					public void audioPlayerBeginInterruption(AVAudioPlayer player) {
-					}
+                    public void audioPlayerBeginInterruption(AVAudioPlayer player) {
+                    }
 
-					public void audioPlayerEndInterruption(AVAudioPlayer player) {
-					}
-				});
-				setToolbarItems(new ArrayList<UIBarButtonItem>(buttonsPauseStop));
-			}
-		});
-		UIBarButtonItem pauseButton = new UIBarButtonItem(UIBarButtonSystemItem.Pause, new UIBarButtonItemDelegate() {
-			public void clicked() {
-				Main.getAudioPlayer().pause();
-				setToolbarItems(new ArrayList<UIBarButtonItem>(buttonsPlayStop));
-			}
-		});
-		UIBarButtonItem stopButton = new UIBarButtonItem(UIBarButtonSystemItem.Stop, new UIBarButtonItemDelegate() {
-			public void clicked() {
-				Main.getAudioPlayer().stop();
-				Main.getAudioPlayer().setCurrentTime(0d);
-				Main.getAudioPlayer().prepareToPlay();
-				setToolbarItems(new ArrayList<UIBarButtonItem>(buttonsPlay));
-			}
-		});
+                    public void audioPlayerEndInterruption(AVAudioPlayer player) {
+                    }
+                });
+                setToolbarItems(new ArrayList<UIBarButtonItem>(buttonsPauseStop));
+            }
+        });
+        UIBarButtonItem pauseButton = new UIBarButtonItem(UIBarButtonSystemItem.Pause, new UIBarButtonItemDelegate() {
+            public void clicked() {
+                Main.getAudioPlayer().pause();
+                setToolbarItems(new ArrayList<UIBarButtonItem>(buttonsPlayStop));
+            }
+        });
+        UIBarButtonItem stopButton = new UIBarButtonItem(UIBarButtonSystemItem.Stop, new UIBarButtonItemDelegate() {
+            public void clicked() {
+                stop();
+            }
+        });
 
-		if (Main.library.getMusicInfos(songInfo).size() == 0) {
+        if (Main.library.getMusicInfos(songInfo).size() == 0) {
 
-		} else if (Main.library.getMusicInfos(songInfo).size() == 1) {
-			Main.setAudioPlayer(AVAudioPlayer.audioPlayerWithContentsOfURL(
-					NSURL.fileURLWithPath(Main.library.getMusicInfos(songInfo).get(0).getMusicPath().getPath()), null));
-			Main.getAudioPlayer().prepareToPlay();
-			Main.getAudioPlayer().setNumberOfLoops(0);
-		}
+        } else if (Main.library.getMusicInfos(songInfo).size() == 1) {
+            Main.setAudioPlayer(AVAudioPlayer.audioPlayerWithContentsOfURL(
+                    NSURL.fileURLWithPath(Main.library.getMusicInfos(songInfo).get(0).getMusicPath().getPath()), null));
+            Main.getAudioPlayer().prepareToPlay();
+            Main.getAudioPlayer().setNumberOfLoops(0);
+        }
 
-		repeat = false;
-		repeatButton = new UIBarButtonItem(UIImage.imageNamed("repeat.png"), UIBarButtonItemStyle.Plain,
-				new UIBarButtonItemDelegate() {
-					public void clicked() {
-						if (!repeat) {
-							Main.getAudioPlayer().setNumberOfLoops(-1);
-							repeatButton.setImage(UIImage.imageNamed("no-repeat.png"));
-						} else {
-							Main.getAudioPlayer().setNumberOfLoops(0);
-							repeatButton.setImage(UIImage.imageNamed("repeat.png"));
-						}
-						repeat = !repeat;
-					}
-				});
-		buttonsPlay = Arrays.asList(playButton, repeatButton);
-		buttonsPauseStop = Arrays.asList(pauseButton, stopButton, repeatButton);
-		buttonsPlayStop = Arrays.asList(playButton, stopButton, repeatButton);
-		setToolbarItems(new ArrayList<UIBarButtonItem>(buttonsPlay));
-	}
+        repeat = false;
+        repeatButton = new UIBarButtonItem(UIImage.imageNamed("repeat.png"), UIBarButtonItemStyle.Plain,
+                new UIBarButtonItemDelegate() {
+                    public void clicked() {
+                        if (!repeat) {
+                            Main.getAudioPlayer().setNumberOfLoops(-1);
+                            repeatButton.setImage(UIImage.imageNamed("no-repeat.png"));
+                        } else {
+                            Main.getAudioPlayer().setNumberOfLoops(0);
+                            repeatButton.setImage(UIImage.imageNamed("repeat.png"));
+                        }
+                        repeat = !repeat;
+                    }
+                });
+        buttonsPlay = Arrays.asList(playButton, repeatButton);
+        buttonsPauseStop = Arrays.asList(pauseButton, stopButton, repeatButton);
+        buttonsPlayStop = Arrays.asList(playButton, stopButton, repeatButton);
+        setToolbarItems(new ArrayList<UIBarButtonItem>(buttonsPlay));
+
+        stop();
+    }
+
+    private void stop() {
+        if (Main.getAudioPlayer() != null) {
+            Main.getAudioPlayer().stop();
+            Main.getAudioPlayer().setCurrentTime(0d);
+            if (Main.library.getMusicInfos(songInfo).size() > 1) {
+                Main.setAudioPlayer(null);
+            } else {
+                Main.getAudioPlayer().prepareToPlay();
+            }
+            setToolbarItems(new ArrayList<UIBarButtonItem>(buttonsPlay));
+        }
+    }
 }
--- iphone/src/main/java/my/kinderlieder/RootViewController.java
+++ iphone/src/main/java/my/kinderlieder/RootViewController.java
@@ -1,72 +1,58 @@
 package my.kinderlieder;
 
+import org.xmlvm.iphone.*;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.xmlvm.iphone.NSIndexPath;
-import org.xmlvm.iphone.UIBarButtonItem;
-import org.xmlvm.iphone.UIButton;
-import org.xmlvm.iphone.UIButtonType;
-import org.xmlvm.iphone.UIControl;
-import org.xmlvm.iphone.UIControlDelegate;
-import org.xmlvm.iphone.UIControlEvent;
-import org.xmlvm.iphone.UIControlState;
-import org.xmlvm.iphone.UITableView;
-import org.xmlvm.iphone.UITableViewCell;
-import org.xmlvm.iphone.UITableViewCellStyle;
-import org.xmlvm.iphone.UITableViewDataSource;
-import org.xmlvm.iphone.UITableViewDelegate;
-import org.xmlvm.iphone.UITableViewStyle;
-import org.xmlvm.iphone.UIViewController;
-import org.xmlvm.iphone.UIWindow;
-
 class RootViewController extends RotatingViewController {
-	public RootViewController(final UIWindow window, final List<SongInfo> songInfos) {
-		UITableView mainView = new UITableView(window.getFrame(), UITableViewStyle.Plain);
-		setTitle("Kinderlieder");
-		final UIButton infoButton = UIButton.buttonWithType(UIButtonType.InfoLight);
-		infoButton.addTarget(new UIControlDelegate() {
-			public void raiseEvent(UIControl sender, int uiControlEvent) {
-				UIViewController infoController = new InfoViewController(window);
-				getNavigationController().pushViewController(infoController, true);
-			}
-		}, UIControlEvent.TouchUpInside);
-		final UIButton shopButton = UIButton.buttonWithType(UIButtonType.InfoLight);
-		shopButton.setTitle("Shop", UIControlState.Normal);
-		shopButton.addTarget(new UIControlDelegate() {
-			public void raiseEvent(UIControl sender, int uiControlEvent) {
-				UIViewController shopController = new ShopViewController(window);
-				getNavigationController().pushViewController(shopController, true);
-			}
-		}, UIControlEvent.TouchUpInside);
-		setToolbarItems(new ArrayList<UIBarButtonItem>(Arrays.asList(new UIBarButtonItem(infoButton),
-				new UIBarButtonItem(shopButton))));
-		setView(mainView);
-		mainView.setDataSource(new UITableViewDataSource() {
-			@Override
-			public UITableViewCell cellForRowAtIndexPath(UITableView table, NSIndexPath idx) {
+    public RootViewController(final UIWindow window, final List<SongInfo> songInfos) {
+        UITableView mainView = new UITableView(window.getFrame(), UITableViewStyle.Plain);
+        setTitle("Kinderlieder");
+        final UIButton infoButton = UIButton.buttonWithType(UIButtonType.InfoLight);
+        infoButton.addTarget(new UIControlDelegate() {
+            public void raiseEvent(UIControl sender, int uiControlEvent) {
+                UIViewController infoController = new InfoViewController(window);
+                getNavigationController().pushViewController(infoController, true);
+            }
+        }, UIControlEvent.TouchUpInside);
+        final UIBarButtonItem shopButton = new UIBarButtonItem(UIImage.imageNamed("arrow_down_24.png"), UIBarButtonItemStyle.Plain, new UIBarButtonItemDelegate() {
 
-				UITableViewCell cell = new UITableViewCell(UITableViewCellStyle.Default, null);
-				cell.getTextLabel().setText(songInfos.get(idx.getRow()).getName());
-				return cell;
-			}
+            public void clicked() {
+                UIViewController shopController = new ShopViewController(window);
+                getNavigationController().pushViewController(shopController, true);
+            }
+        });
+        shopButton.setTitle("Download");
 
-			@Override
-			public int numberOfRowsInSection(UITableView table, int section) {
-				return songInfos.size();
-			}
-		});
+        setToolbarItems(new ArrayList<UIBarButtonItem>(Arrays.asList(new UIBarButtonItem(infoButton),
+                shopButton)));
+        setView(mainView);
+        mainView.setDataSource(new UITableViewDataSource() {
+            @Override
+            public UITableViewCell cellForRowAtIndexPath(UITableView table, NSIndexPath idx) {
 
-		mainView.setDelegate(new UITableViewDelegate() {
-			@Override
-			public void didSelectRowAtIndexPath(UITableView tableview, NSIndexPath indexPath) {
-				final SongInfo songInfo = songInfos.get(indexPath.getRow());
-				final UIViewController pdfViewController = new PdfViewVontroller(songInfo, window);
-				getNavigationController().setToolbarHidden(false, true);
-				getNavigationController().pushViewController(pdfViewController, true);
-			}
-		});
+                UITableViewCell cell = new UITableViewCell(UITableViewCellStyle.Default, null);
+                cell.getTextLabel().setText(songInfos.get(idx.getRow()).getName());
+                return cell;
+            }
 
-	}
+            @Override
+            public int numberOfRowsInSection(UITableView table, int section) {
+                return songInfos.size();
+            }
+        });
+
+        mainView.setDelegate(new UITableViewDelegate() {
+            @Override
+            public void didSelectRowAtIndexPath(UITableView tableview, NSIndexPath indexPath) {
+                final SongInfo songInfo = songInfos.get(indexPath.getRow());
+                final UIViewController pdfViewController = new PdfViewVontroller(songInfo, window);
+                getNavigationController().setToolbarHidden(false, true);
+                getNavigationController().pushViewController(pdfViewController, true);
+            }
+        });
+
+    }
 }
